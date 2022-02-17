5603bc8bfb41de3c50b9c98558a7384a3f59980c
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-02-16 17:00:41 -0800

    Optimize imports after last commit

--- app/src/main/java/com/chaveze/bloodpressure/MainActivity.java
+++ app/src/main/java/com/chaveze/bloodpressure/MainActivity.java
@@ -1,19 +1,14 @@
 package com.chaveze.bloodpressure;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
