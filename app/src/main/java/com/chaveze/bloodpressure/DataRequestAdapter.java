f5ebbc7d8f42893125a2319ddbb62f028ec5b2ca
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-02-19 10:37:54 -0800

    Add format to list, see commit description for details
    Add constants for min & max pressure values.
    Hide/show exclamation icon, change time position.
    Update font color for pressure values.
    Add line separator.

--- app/src/main/java/com/chaveze/bloodpressure/DataRequestAdapter.java
+++ app/src/main/java/com/chaveze/bloodpressure/DataRequestAdapter.java
@@ -1,5 +1,6 @@
 package com.chaveze.bloodpressure;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,18 @@ import java.util.concurrent.TimeUnit;
 
 public class DataRequestAdapter extends RecyclerView.Adapter<DataRequestAdapter.ViewHolder> {
     final String TAG = "DataRequestAdapter";
+
+    // Values according to
+    // https://www.nhs.uk/common-health-questions/lifestyle/what-is-blood-pressure/
+    final int MAX_IDEAL_SYS_PRESSURE = 120;
+    final int MAX_IDEAL_DIA_PRESSURE = 80;
+    final int MIN_IDEAL_SYS_PRESSURE = 90;
+    final int MIN_IDEAL_DIA_PRESSURE = 60;
+
+    final int UNSAFE_HIGH_SYS_PRESSURE = 140;
+    final int UNSAFE_HIGH_DIA_PRESSURE = 90;
+    final int UNSAFE_LOW_SYS_PRESSURE = MIN_IDEAL_SYS_PRESSURE;
+    final int UNSAFE_LOW_DIA_PRESSURE = MIN_IDEAL_DIA_PRESSURE;
 
     DataSet entriesSet = null;
 
@@ -89,18 +102,46 @@ public class DataRequestAdapter extends RecyclerView.Adapter<DataRequestAdapter.
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
 
         DataPoint dp = entriesSet.getDataPoints().get(position);
-        LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(dp.getStartTime(TimeUnit.SECONDS)), ZoneId.systemDefault());
-        String date = dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
-        viewHolder.getTextView(TXT.DATE).setText(date);
-        String time = dateTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
-        viewHolder.getTextView(TXT.TIME).setText(time);
 
+        // TODO To be moved into a more appropriate class or method
+        boolean inSafeZone = true;
         for (Field field : dp.getDataType().getFields()) {
             if (field.getName().contains("sys")) {
-                viewHolder.getTextView(TXT.SYS).setText(dp.getValue(field).toString());
+                float sysPressure = dp.getValue(field).asFloat();
+
+                viewHolder.getTextView(TXT.SYS).setText("Sys: "+dp.getValue(field).toString());
+                if (sysPressure < MIN_IDEAL_SYS_PRESSURE || sysPressure > MAX_IDEAL_SYS_PRESSURE)
+                    inSafeZone = false;
             } else if (field.getName().contains("dia")) {
-                viewHolder.getTextView(TXT.DIA).setText(dp.getValue(field).toString());
+                float diaPressure = dp.getValue(field).asFloat();
+
+                viewHolder.getTextView(TXT.DIA).setText("Dia: "+dp.getValue(field).toString());
+                if (diaPressure < MIN_IDEAL_DIA_PRESSURE || diaPressure > MAX_IDEAL_DIA_PRESSURE)
+                    inSafeZone = false;
             }
+        }
+
+        if (inSafeZone) {
+            viewHolder.getTextView(TXT.SYS).setTextColor(Color.WHITE);
+            viewHolder.getTextView(TXT.DIA).setTextColor(Color.WHITE);
+        } else {
+            viewHolder.getTextView(TXT.SYS).setTextColor(Color.RED);
+            viewHolder.getTextView(TXT.DIA).setTextColor(Color.RED);
+        }
+
+        LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(dp.getStartTime(TimeUnit.SECONDS)), ZoneId.systemDefault());
+        String date = dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
+        String time = dateTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
+
+        if (inSafeZone) {
+            viewHolder.getTextView(TXT.DATE).setText(date);
+            viewHolder.getTextView(TXT.TIME).setText(time);
+            viewHolder.getTextView(TXT.TIME).setVisibility(View.VISIBLE);
+            viewHolder.getImageView(IMG.UNSAFE).setVisibility(View.GONE);
+        } else {
+            viewHolder.getTextView(TXT.DATE).setText(date + "\n" + time);
+            viewHolder.getTextView(TXT.TIME).setVisibility(View.GONE);
+            viewHolder.getImageView(IMG.UNSAFE).setVisibility(View.VISIBLE);
         }
     }
 
